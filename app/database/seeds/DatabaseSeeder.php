c030ef3f5a663929bbbca22678af53a80f9fd98c
Troy Pavlek <Tpavlek@ualberta.ca>
2014-03-21 13:48:51 -0400

    Wow

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -2,16 +2,34 @@
 
 class DatabaseSeeder extends Seeder {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Eloquent::unguard();
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
-	}
+    $this->call('PersonUserSeeder');
+  }
 
+}
+
+class PersonUserSeeder extends Seeder {
+
+  public function run() {
+    $person = Person::create(array('first_name' => "Troy",
+                         'last_name' => 'Pavlek',
+                         'address' => "edmonton",
+                         'email' => "tpavlek@ualberta.ca",
+                         'phone' => "17802003304"
+          ));
+
+    $user = User::create(array('user_name' => 'tpavlek',
+                               'password' => Hash::make("wow"),
+                               'class' => 'a',
+                               'date_registered' => new DateTime('NOW'),
+          ));
+  }
 }
