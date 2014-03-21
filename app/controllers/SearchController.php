9d2e21a62bf107ad156a7f7050e2cd41c6f848a3
Douglas Schneider <ds3@ualberta.ca>
2014-03-20 19:46:17 -0600

    add order by to search module

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -15,9 +15,17 @@ class SearchController extends BaseController {
         if($query == "" && $startDate == "" && $endDate == "")
             return Redirect::route("search")->withErrors(array("Must include a search term or date range."));
 
-        $statement = "select *
-                      from oralism.radiology_record r, oralism.persons p
-                      where r.patient_id = p.person_id";
+        $statement = "select * ";
+        if($query != "")
+            $statement .= ", match(p.first_name,p.last_name)
+                             against ('{$query}' in boolean mode) as patient_score,
+                             match(r.description)
+                             against ('{$query}' in boolean mode) as description_score,
+                             match(r.diagnosis)
+                             against ('{$query}' in boolean mode) as diagnosis_score ";
+
+        $statement .= "from oralism.radiology_record r, oralism.persons p
+                       where r.patient_id = p.person_id";
 
         if($query != "")
             $statement .= " and (match(p.first_name,p.last_name)
@@ -31,6 +39,9 @@ class SearchController extends BaseController {
 
         if($endDate != "")
             $statement .= " and r.test_date <= '{$endDate}'";
+
+        if($query)
+            $statement .= " order by (6*patient_score + 3*diagnosis_score + description_score) ";
 
         return DB::select($statement);
     }
