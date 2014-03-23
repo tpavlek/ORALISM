383c16665e4a7839467fc3c0b8a615be84ccd3d2
Douglas Schneider <ds3@ualberta.ca>
2014-03-23 16:18:02 -0600

    added the documentation for the login and search modules.
    Added a documentation section for installation

--- app/views/docs.md
+++ app/views/docs.md
@@ -1,20 +1,115 @@
-ORALISM Documentation
+User Documentation
 =====================
 
 Login Module
 ------------
 
+To get to the login page you can press the Log in button on the navigation bar.
+
+#### Logging in
+
+To login first proceed to the login page by pressing the Log in button on the
+navigation bar. Then enter your username and password in the text boxes
+labelled UserName and Password respectively. Clicking the button labelled LOGIN
+will log you in if the given login credentials are correct.
+
+#### Invalid login credentials
+
+If the login credentials provided are incorrect then you will be taken
+back to the login page and there will be a message displaying that
+the provided login credentials were invalid.
+
+#### Logging out
+
+To logout you can click the Log out <username> button on the navigation bar.
+This will log you out, and take you back to the home screen.
+
 User Management Module
 ----------------------
+
+TODO
 
 Report Generating Module
 ------------------------
 
+TODO
+
 Uploading Module
 ----------------
+
+TODO
 
 Search Module
 -------------
 
+While performing searches there are two main pages. The first page
+is the search page where a query is input. The second page is the
+results page where the results of a search are displayed.
+
+#### Making a search
+
+To get to the search page, click the Search button on the navigation bar.
+To make a query against existing records, type the query into the textbox
+labelled search and click the button labelled SEARCH. This will take you to the
+search results page (explained below).
+
+#### Limiting a search based on dates
+
+The query can be narrowed down by specifying the start and end date. If only
+the start date is specified then all records matching the query after that date
+will be displayed. If only the end date is specified then all records matching
+the query before that date will be displayed. If both are specified then only
+records matching the query that fall between those two dates will be displayed.
+
+The dates selected are inclusive in the search results. Meaning that the
+records displayed will be from the start date, inclusive, to the end date, also
+inclusive.
+
+It is possible to leave the query blank and only select dates. If this is
+done then the search results will contain all records between the selected
+dates, based on the description above.
+
+#### Search result ordering
+
+There are three different ways to order the results. These can be selected
+from the radio buttons below the date entry.
+
+The first method is by relevancy. If this button is selected then the results
+will be displayed based on the relevancy of the record to the search query. If
+the search query is left blank then the results have no guaranteed ordering.
+
+The second and third method are most-recent-first, and most-recent-last. These
+two methods order the results based on test date. The records with the most
+recent and least recent test dates are displayed first respectively.
+
+#### Viewing search results
+
+Search results are displayed on a separate page after clicking the search
+button on the search page. If the given query matches no records then the
+text "No Results Found!" will be displayed. Otherwise a table of matching
+records will be displayed. The table displays the patient, doctor,
+radiologist, test type, prescribing date, test date, diagnosis, description,
+and thumbnails.
+
+#### Viewing record images
+
+There are two ways to view record images from the search results page. The
+first method is to click on the images thumbnail in the search results table.
+This will open a new window containing the image. The two buttons can be used
+to increase or decrease the size of the image.
+
+The second method to view a records images is to click on the "View" button in
+the same row as the record you want to view images for. This will take you to
+the records page. At the bottom of this page is a list of images for the
+record. Again the two buttons can be used to increase or decrease the size of
+the image.
+
 Data Analysis Module
 --------------------
+
+TODO
+
+Installation Instructions
+=========================
+
+TODO
--- app/views/docs.php
+++ app/views/docs.php
@@ -1,13 +1,107 @@
-<h1>ORALISM Documentation</h1>
+<h1>User Documentation</h1>
 
 <h2>Login Module</h2>
 
+<p>To get to the login page you can press the Log in button on the navigation bar.</p>
+
+<h4>Logging in</h4>
+
+<p>To login first proceed to the login page by pressing the Log in button on the
+navigation bar. Then enter your username and password in the text boxes
+labelled UserName and Password respectively. Clicking the button labelled LOGIN
+will log you in if the given login credentials are correct.</p>
+
+<h4>Invalid login credentials</h4>
+
+<p>If the login credentials provided are incorrect then you will be taken
+back to the login page and there will be a message displaying that
+the provided login credentials were invalid.</p>
+
+<h4>Logging out</h4>
+
+<p>To logout you can click the Log out <username> button on the navigation bar.
+This will log you out, and take you back to the home screen.</p>
+
 <h2>User Management Module</h2>
+
+<p>TODO</p>
 
 <h2>Report Generating Module</h2>
 
+<p>TODO</p>
+
 <h2>Uploading Module</h2>
+
+<p>TODO</p>
 
 <h2>Search Module</h2>
 
+<p>While performing searches there are two main pages. The first page
+is the search page where a query is input. The second page is the
+results page where the results of a search are displayed.</p>
+
+<h4>Making a search</h4>
+
+<p>To get to the search page, click the Search button on the navigation bar.
+To make a query against existing records, type the query into the textbox
+labelled search and click the button labelled SEARCH. This will take you to the
+search results page (explained below).</p>
+
+<h4>Limiting a search based on dates</h4>
+
+<p>The query can be narrowed down by specifying the start and end date. If only
+the start date is specified then all records matching the query after that date
+will be displayed. If only the end date is specified then all records matching
+the query before that date will be displayed. If both are specified then only
+records matching the query that fall between those two dates will be displayed.</p>
+
+<p>The dates selected are inclusive in the search results. Meaning that the
+records displayed will be from the start date, inclusive, to the end date, also
+inclusive.</p>
+
+<p>It is possible to leave the query blank and only select dates. If this is
+done then the search results will contain all records between the selected
+dates, based on the description above.</p>
+
+<h4>Search result ordering</h4>
+
+<p>There are three different ways to order the results. These can be selected
+from the radio buttons below the date entry.</p>
+
+<p>The first method is by relevancy. If this button is selected then the results
+will be displayed based on the relevancy of the record to the search query. If
+the search query is left blank then the results have no guaranteed ordering.</p>
+
+<p>The second and third method are most-recent-first, and most-recent-last. These
+two methods order the results based on test date. The records with the most
+recent and least recent test dates are displayed first respectively.</p>
+
+<h4>Viewing search results</h4>
+
+<p>Search results are displayed on a separate page after clicking the search
+button on the search page. If the given query matches no records then the
+text "No Results Found!" will be displayed. Otherwise a table of matching
+records will be displayed. The table displays the patient, doctor,
+radiologist, test type, prescribing date, test date, diagnosis, description,
+and thumbnails.</p>
+
+<h3>Viewing record images</h3>
+
+<p>There are two ways to view record images from the search results page. The
+first method is to click on the images thumbnail in the search results table.
+This will open a new window containing the image. The two buttons can be used
+to increase or decrease the size of the image.</p>
+
+<p>The second method to view a records images is to click on the "View" button in
+the same row as the record you want to view images for. This will take you to
+the records page. At the bottom of this page is a list of images for the
+record. Again the two buttons can be used to increase or decrease the size of
+the image.</p>
+
 <h2>Data Analysis Module</h2>
+
+<p>TODO</p>
+
+<h1>Installation Instructions</h1>
+
+<p>TODO</p>
