a7fd5393f0943dd158408fa55b3b5c6e4b672f7a
Douglas Schneider <ds3@ualberta.ca>
2014-03-24 13:37:42 -0600

    add the docs for the User Management Module

--- app/views/docs.md
+++ app/views/docs.md
@@ -27,7 +27,77 @@ This will log you out, and take you back to the home screen.
 User Management Module
 ----------------------
 
-TODO
+The User Management Module allows administrators to add new users, and
+modify existing users. This page is accessible only while logged in
+as an administrator. It can be accessed by clicking on the
+"User Management" button on the navigation bar.
+
+#### Adding new users
+
+A new user can be added to the system by clicking the "NEW USER" button
+on the User Management page. Clicking this button will take you to a new
+page where the new users information can be entered.
+
+The new user page requires a first name, last name, address, email,
+phone number, username, password, and class. All fields are required. 
+The email must be in the form <user>@<domain>.<extension>. The phone
+number must be 10 digits long (e.g. 123-456-7890).
+
+Clicking the "SAVE" button will create the new user with a login
+with the given username, password and class. After this, new logins
+can be created using the method described below.
+
+Clicking the "CANCEL" button will not create a new user, and will return to the
+User Management screen.
+
+#### Creating new logins
+
+Users can have multiple logins. In order to add a new login to a user,
+a user must first be created using the above method.
+
+To add a new login to a user, first find the user on the User Management page.
+Then click the "NEW LOGIN" button beside that users name. This will take
+you to a new page. On the new page you must enter the new username, password,
+and select the class of the new login. All of the users old logins will still
+remain, upon the creation of the new login. To create the new login, click the
+"SAVE" button. To cancel the new login, click the "CANCEL" button.
+
+#### Editing a user
+
+A user can be edited by click on the "EDIT" button beside their name
+in the table of users on the User Management page.
+
+The edit user page can also be reached by clicking the "Edit Info" button
+on the navigation bar. However, that will only allow you to modify the user
+info of the user you are currently logged into.
+
+On the edit user page, the name, address, email, and phone number of the
+user can be modified. In addition the username, password, and class of
+any of the users logins can be changed.
+
+After making any changes to the user, the "SAVE" button must be clicked
+for the changes to take affect.
+
+#### Managing doctors
+
+To manage doctors of a person, click the "MANAGE DOCTORS" button beside
+that persons name on the User Management page. This will take you to a new
+page where you can remove existing doctors, and add new doctors.
+
+##### Adding new doctors to a person
+
+Once on the Manage Doctors page. You can give the person a new family
+doctor by selecting the doctor from the drop down list and clicking "SAVE".
+This will add the doctor to the patient. Clicking "CANCEL" will take you back
+to the User Management page.
+
+##### Removing doctors from a person
+
+If a person has family doctors, they can be removed from the Manage
+Doctors page. To remove a family doctor, find the doctor in the table
+of family doctors. Clicking the "REMOVE" button will remove the family
+doctor from that person. The list of family doctors will only be visible
+if the selected person have family doctors assigned to them.
 
 Report Generating Module
 ------------------------
--- app/views/docs.php
+++ app/views/docs.php
@@ -24,7 +24,77 @@ This will log you out, and take you back to the home screen.</p>
 
 <h2>User Management Module</h2>
 
-<p>TODO</p>
+<p>The User Management Module allows administrators to add new users, and
+modify existing users. This page is accessible only while logged in
+as an administrator. It can be accessed by clicking on the
+"User Management" button on the navigation bar.</p>
+
+<h4>Adding new users</h4>
+
+<p>A new user can be added to the system by clicking the "NEW USER" button
+on the User Management page. Clicking this button will take you to a new
+page where the new users information can be entered.</p>
+
+<p>The new user page requires a first name, last name, address, email,
+phone number, username, password, and class. All fields are required. 
+The email must be in the form <user>@<domain>.<extension>. The phone
+number must be 10 digits long (e.g. 123-456-7890).</p>
+
+<p>Clicking the "SAVE" button will create the new user with a login
+with the given username, password and class. After this, new logins
+can be created using the method described below.</p>
+
+<p>Clicking the "CANCEL" button will not create a new user, and will return to the
+User Management screen.</p>
+
+<h4>Creating new logins</h4>
+
+<p>Users can have multiple logins. In order to add a new login to a user,
+a user must first be created using the above method.</p>
+
+<p>To add a new login to a user, first find the user on the User Management page.
+Then click the "NEW LOGIN" button beside that users name. This will take
+you to a new page. On the new page you must enter the new username, password,
+and select the class of the new login. All of the users old logins will still
+remain, upon the creation of the new login. To create the new login, click the
+"SAVE" button. To cancel the new login, click the "CANCEL" button.</p>
+
+<h4>Editing a user</h4>
+
+<p>A user can be edited by click on the "EDIT" button beside their name
+in the table of users on the User Management page.</p>
+
+<p>The edit user page can also be reached by clicking the "Edit Info" button
+on the navigation bar. However, that will only allow you to modify the user
+info of the user you are currently logged into.</p>
+
+<p>On the edit user page, the name, address, email, and phone number of the
+user can be modified. In addition the username, password, and class of
+any of the users logins can be changed.</p>
+
+<p>After making any changes to the user, the "SAVE" button must be clicked
+for the changes to take affect.</p>
+
+<h4>Managing doctors</h4>
+
+<p>To manage doctors of a person, click the "MANAGE DOCTORS" button beside
+that persons name on the User Management page. This will take you to a new
+page where you can remove existing doctors, and add new doctors.</p>
+
+<h5>Adding new doctors to a person</h5>
+
+<p>Once on the Manage Doctors page. You can give the person a new family
+doctor by selecting the doctor from the drop down list and clicking "SAVE".
+This will add the doctor to the patient. Clicking "CANCEL" will take you back
+to the User Management page.</p>
+
+<h5>Removing doctors from a person</h5>
+
+<p>If a person has family doctors, they can be removed from the Manage
+Doctors page. To remove a family doctor, find the doctor in the table
+of family doctors. Clicking the "REMOVE" button will remove the family
+doctor from that person. The list of family doctors will only be visible
+if the selected person have family doctors assigned to them.</p>
 
 <h2>Report Generating Module</h2>
 
@@ -85,7 +155,7 @@ records will be displayed. The table displays the patient, doctor,
 radiologist, test type, prescribing date, test date, diagnosis, description,
 and thumbnails.</p>
 
-<h3>Viewing record images</h3>
+<h4>Viewing record images</h4>
 
 <p>There are two ways to view record images from the search results page. The
 first method is to click on the images thumbnail in the search results table.
